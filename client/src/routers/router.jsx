8277f661613ee7b9acbc56dcecc33c905b770561
Yugo <ilhamyugooo@gmail.com>
2024-05-21 20:49:28 +0700

    register

---
+++ client/src/routers/router.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter
+} from "react-router-dom";
+import Register from '../views/Register';
+
+const router = createBrowserRouter([
+    {
+        path: "/register",
+        element: <Register />,
+    }
+]);
+
+export default router
