86c5e1c0f48db4f57ce56c0fb015d07ba96795b7
makbara000 <makbara000@gmail.com>
2024-05-22 14:57:29 +0700

    Real time game score

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 
 export const serverApi = axios.create({
+<<<<<<< HEAD
     // baseURL: "http://localhost:3000"
+=======
+>>>>>>> 15cfb1e3c6261eeaa565330c96f67ee4a4c56dfb
     baseURL: "https://server-typing-game.yugoboy.online"
 })
--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,4 +1,8 @@
 import { io } from "socket.io-client";
 
+<<<<<<< HEAD
 // export const socket = io("http://localhost:3000");
 export const socket = io("https://server-typing-game.yugoboy.online");
+=======
+export const socket = io("https://server-typing-game.yugoboy.online");
+>>>>>>> 15cfb1e3c6261eeaa565330c96f67ee4a4c56dfb
