a7699b52411ea5d0b05a1e36ce26ba9d9b17e8d9
makbara000 <makbara000@gmail.com>
2024-05-22 12:11:18 +0700

    Extra Context

--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -1,10 +1,15 @@
 import ChatApp from "../components/ChatApp";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { useContext } from "react";
 
 export default function HomePage() {
+  const { theme, setTheme, currentTheme} = useContext(ThemeContext)
   return (
     <>
-      <h1>Home Page</h1>
+    <div className={`${theme[currentTheme].bg} `} style={{height: "100vh"}}>
+      <h1 className={`${theme[currentTheme].text} `}>Home Page</h1>
       <ChatApp />
+    </div>
     </>
   );
 }
